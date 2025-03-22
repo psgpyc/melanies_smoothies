2f102c3697f2ec69008d4a6f8dffbff726c61ad3
Paritosh Sharma Ghimire <psgpyc@gmail.com>
2025-03-22 08:38:57 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 # Import python packages
+
 import streamlit as st
 from snowflake.snowpark.functions import col
+
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
 
 
 cnx = st.connection("snowflake")
